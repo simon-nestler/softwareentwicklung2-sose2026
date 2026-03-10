568041094751ef6b273e5d62f748230931746d98
Simon Nestler <simon.nestler@thi.de>
2026-03-10 20:10:39 +0100

    Musterlösung da

--- Beispielprojekt/src/App.java
+++ Beispielprojekt/src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Vorlesung startet...");
+
+        System.out.printlin("Die offizielle Musterlösung");
+
     }
 }
