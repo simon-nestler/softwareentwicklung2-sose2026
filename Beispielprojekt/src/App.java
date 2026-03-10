9186f4b410adb71eb36a52c4cb3348961cfb0793
Simon Nestler <simon.nestler@thi.de>
2026-03-10 20:13:36 +0100

    Fortsetzung

--- Beispielprojekt/src/App.java
+++ Beispielprojekt/src/App.java
@@ -3,7 +3,7 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Vorlesung startet...");
 
-        System.out.printlin("Die offizielle Musterlösung");
-
+        System.out.println("Die offizielle Musterlösung");
+        System.out.println("Die Vorlesung geht weiter...");
     }
 }
