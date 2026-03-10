932627d3785e2ef873736d5484b4e668b4695799
Simon Nestler <simon.nestler@thi.de>
2026-03-10 19:57:08 +0100

    Zwischenstand nach der ersten Uebung.

--- Beispielprojekt/src/App.java
+++ Beispielprojekt/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Vorlesung startet jetzt...");
+
+        System.out.printlin("Das ist meine eigene Lösung.");
     }
 }
