6c3b7865a0b680817fa7c375400ab72e2dc9ae93
Simon Nestler <simon.nestler@thi.de>
2026-03-10 19:54:10 +0100

    Vorlesung startet...

--- Beispielprojekt/src/App.java
+++ Beispielprojekt/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Dies ist ein Beispielprojekt.");
+        System.out.println("Vorlesung startet...");
     }
 }
