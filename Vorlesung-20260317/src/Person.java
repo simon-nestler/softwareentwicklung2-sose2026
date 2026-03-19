5cbeaa012b88706b4acabc796d5f3b1770ec58b5
Simon Nestler <simon.nestler@thi.de>
2026-03-19 10:34:57 +0100

    Stand Ende Vorlesung 20260319

--- Vorlesung-20260317/src/Person.java
+++ Vorlesung-20260317/src/Person.java
@@ -1,5 +1,6 @@
 public class Person {
     private String name;
+    private boolean istFahrer = false;
 
     public Person(String name) {
         this.name = name;
@@ -7,5 +8,23 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    public boolean getIstFahrer() {
+        return istFahrer;
+    }
+
+    public void setIstFahrer(boolean istFahrer) {
+        this.istFahrer = istFahrer;
+    }
+
+    // oder:
+
+    public void setIstFahrer() {
+        this.istFahrer = true;
+    }
+
+    public void setIstMitfahrer() {
+        this.istFahrer = false;
     }
 }
