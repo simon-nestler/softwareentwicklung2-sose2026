4d3929c24b610a1a7448167676c6f7f2bf87c90d
Simon Nestler <simon.nestler@thi.de>
2026-03-26 09:22:42 +0100

    loesung uebung 1

--- Vorlesung-20260326/src/App.java
+++ Vorlesung-20260326/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Button button1 = new Button();
+        Button button2 = new Button(100, 100);
+        Button button3 = new Button(0, 0, 100, 100);
     }
 }
