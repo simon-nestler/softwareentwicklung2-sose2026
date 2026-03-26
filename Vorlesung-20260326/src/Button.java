7e75ed0b7c9e3c852d77bc39ba4d6eb92aa51223
Simon Nestler <simon.nestler@thi.de>
2026-03-26 09:08:45 +0100

    Drei Konstruktoren fuer die Buttons

--- Vorlesung-20260326/src/Button.java
+++ Vorlesung-20260326/src/Button.java
@@ -5,6 +5,14 @@ public class Button {
     private int breite;
 
     public Button() {
+        System.out.println("Standard-Konstruktor.");
+    }
 
+    public Button(int breite, int hoehe) {
+        System.out.println("Konstruktor mit Breite und Höhe.");
+    }
+
+    public Button(int x, int y, int breite, int hoehe) {
+        System.out.println("Konstruktor mit X, Y, Breite und Höhe.");
     }
 }
