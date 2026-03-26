c3db5d78e3159b5468901b7eeb4b977c85e7cd20
Simon Nestler <simon.nestler@thi.de>
2026-03-26 09:24:12 +0100

    konstruktoren setzen instanzvariablen

--- Vorlesung-20260326/src/Button.java
+++ Vorlesung-20260326/src/Button.java
@@ -1,8 +1,8 @@
 public class Button {
-    private int x;
-    private int y;
-    private int hohe;
-    private int breite;
+    private int x = 0;
+    private int y = 0;
+    private int hohe = 50;
+    private int breite = 50;
 
     public Button() {
         System.out.println("Standard-Konstruktor.");
@@ -10,9 +10,15 @@ public class Button {
 
     public Button(int breite, int hoehe) {
         System.out.println("Konstruktor mit Breite und Höhe.");
+        this.breite = breite;
+        this.hohe = hoehe;
     }
 
     public Button(int x, int y, int breite, int hoehe) {
         System.out.println("Konstruktor mit X, Y, Breite und Höhe.");
+        this.x = x;
+        this.y = y;
+        this.breite = breite;
+        this.hohe = hoehe;
     }
 }
