a0883019d292176acbacc92290bb6138e165b313
Simon Nestler <simon.nestler@thi.de>
2026-04-09 10:11:27 +0200

    aktuelles datum ausgeben

--- Vorlesung-20260409/src/App.java
+++ Vorlesung-20260409/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Ausgabe.systemInformationen();
         // Ausgabe.weitereAusgaben();
-        Ausgabe.arbeitenMitPrintF();
+        // Ausgabe.arbeitenMitPrintF();
+        Ausgabe.uhrzeitUndDatum();
     }
 }
--- Vorlesung-20260409/src/Ausgabe.java
+++ Vorlesung-20260409/src/Ausgabe.java
@@ -1,3 +1,6 @@
+import java.util.Date;
+import java.util.Calendar;
+
 public class Ausgabe {
     public static void systemInformationen() {
         System.out.println("User Home-Verzeichnis: " + System.getProperty("user.home"));
@@ -15,13 +18,20 @@ public class Ausgabe {
 
     public static void arbeitenMitPrintF() {
         String name1 = "Softwareentwicklung 1";
-        String name2 = "Softwareentwicklung 2";
+        String name2 = "SE 2";
         double preis1 = 18.80;
         double preis2 = 9.80;
 
         System.out.println("Das Buch " + name1 + " kostet " + preis1 + " Euro.");
 
-        System.out.printf("Das Buch %s kostet %6.2f Euro.%n", name1, preis1);
-        System.out.printf("Das Buch %s kostet %6.2f Euro.%n", name2, preis2);
+        System.out.printf("Das Buch %30s kostet %6.2f Euro.%n", name1, preis1);
+        System.out.printf("Das Buch %30s kostet %6.2f Euro.%n", name2, preis2);
+    }
+
+    public static void uhrzeitUndDatum() {
+        Date time = Calendar.getInstance().getTime();
+        System.out.printf("Aktuelles Datum und Uhrzeit: %tc%n", time);
+
+        System.out.println("Heute ist der 09.04.2026.");
     }
 }
