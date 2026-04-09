2f0df3e22403a8c7aa1159fc801e145c9c4b95a0
Simon Nestler <simon.nestler@thi.de>
2026-04-09 09:45:02 +0200

    Besondere Zeichen ausgeben

--- Vorlesung-20260409/src/App.java
+++ Vorlesung-20260409/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Ausgabe.systemInformationen();
+        // Ausgabe.systemInformationen();
+        Ausgabe.weitereAusgaben();
     }
 }
--- Vorlesung-20260409/src/Ausgabe.java
+++ Vorlesung-20260409/src/Ausgabe.java
@@ -1,7 +1,15 @@
 public class Ausgabe {
     public static void systemInformationen() {
+        System.out.println("User Home-Verzeichnis: " + System.getProperty("user.home"));
         System.out.println("Betriebssystem: " + System.getProperty("os.name"));
+        System.out.println("Betriebssystem-Version: " + System.getProperty("os.version"));
         System.out.println("Benutzername: " + System.getProperty("user.name"));
         System.out.println("Java-Version: " + System.getProperty("java.version"));
+        System.out.println("Arbeitsverzeichnis: " + System.getProperty("java.class.path"));
+    }
+
+    public static void weitereAusgaben() {
+        System.out.println("Mein Name ist: \t\t \"Simon Nestler.\"");
+        System.out.println("Nach \\n kommt eine neue Zeile \nJetzt neue Zeile.");
     }
 }
