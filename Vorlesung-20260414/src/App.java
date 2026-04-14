59a277dc0fdbafd83e0b180f0f7391c941c51237
Simon Nestler <simon.nestler@thi.de>
2026-04-14 12:21:23 +0200

    Scanner und BufferedReader

--- Vorlesung-20260414/src/App.java
+++ Vorlesung-20260414/src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        InputOutput.schreibenAufKonsole();
-        InputOutput.inDateiSchreiben();
+        // InputOutput.schreibenAufKonsole();
+        // InputOutput.inDateiSchreiben();
+
+        // InputOutput.lesenVonKonsoleMitSystemIn();
+        // InputOutput.lesenVonKonsoleMitScanner();
+        InputOutput.lesenVonKonsoleMitBufferedReader();
     }
 }
--- Vorlesung-20260414/src/InputOutput.java
+++ Vorlesung-20260414/src/InputOutput.java
@@ -1,10 +1,43 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class InputOutput {
     public static void schreibenAufKonsole() {
         PrintWriter writer = new PrintWriter(System.out, true);
         schreiben(writer);
+    }
+
+    public static void lesenVonKonsoleMitSystemIn() {
+        try {
+            int i = System.in.read();
+            System.out.println("Gelesenes Zeichen: " + (char) i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void lesenVonKonsoleMitScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geben Sie Ihren Namen ein: ");
+        String name = scanner.nextLine();
+        System.out.println("Hallo, " + name + "!");
+        scanner.close();
+    }
+
+    public static void lesenVonKonsoleMitBufferedReader() {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        System.out.print("Geben Sie Ihren Namen ein: ");
+        try {
+            String name = reader.readLine();
+            System.out.println("Hallo, " + name + "!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void inDateiSchreiben() {
@@ -18,7 +51,7 @@ public class InputOutput {
     }
 
     private static void schreiben(PrintWriter writer) {
-        writer.println("Hello, World! \n Was für ein schöner Tag!");
+        writer.println("Hello, World! \nWas für ein schöner Tag!");
         writer.printf("The value of pi is approximately %.2f%n", Math.PI);
         writer.close();
     }
