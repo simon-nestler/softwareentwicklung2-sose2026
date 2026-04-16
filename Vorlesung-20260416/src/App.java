59deb148f4b17ef3dfe77d178b3524ddd9b7fb31
Simon Nestler <simon.nestler@thi.de>
2026-04-16 09:47:06 +0200

    Dummy fuer Grossmutter und Urgrossmutter

--- Vorlesung-20260416/src/App.java
+++ Vorlesung-20260416/src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Person p1 = new Person("Max");
-        p1.printPerson();
+
+        System.out.println("Name der Mutter: " + p1.getMutter().getName());
+        System.out.println("------");
+        System.out.println(p1.printPerson());
+
     }
 }
--- Vorlesung-20260416/src/Person.java
+++ Vorlesung-20260416/src/Person.java
@@ -4,14 +4,35 @@ public class Person {
 
     public Person(String name) {
         this.name = name;
+
+        if (this.name.length() < 100) {
+            this.mutter = new Person("Mutter von " + name);
+        }
     }
 
-    public void printPerson() {
-        System.out.print("Name: " + name + ", ");
+    public Person getGrossMutter() {
+        return null;
+    }
+
+    public Person getUrGrossMutter() {
+        return null;
+    }
+
+    public Person getMutter() {
+        return mutter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String printPerson() {
+        String result = "Name: " + name + ", ";
         if (mutter != null) {
-            System.out.println("Mutter: " + mutter.name);
+            result += "Mutter: " + mutter.printPerson();
         } else {
-            System.out.println("Keine Mutter angegeben.");
+            result += "Keine Mutter angegeben.";
         }
+        return result;
     }
 }
