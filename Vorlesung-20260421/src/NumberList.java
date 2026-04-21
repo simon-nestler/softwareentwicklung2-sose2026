c960d94b9230bca08ba03dea53566c381dc5cc89
Simon Nestler <simon.nestler@thi.de>
2026-04-21 12:46:04 +0200

    NumberList

---
+++ Vorlesung-20260421/src/NumberList.java
@@ -0,0 +1,20 @@
+public class NumberList {
+    NumberNode head; // 1. Listenelement = Kopf der Liste
+
+    public void add(int value) {
+        NumberNode q = new NumberNode(value);
+        NumberNode p = head;
+        NumberNode prev = null;
+
+        while (p != null) { // Suche bis zum Ende der Liste
+            prev = p;
+            p = p.next;
+        }
+
+        if (prev == null) { // Liste war leer, neues Element wird zum Kopf
+            head = q;
+        } else { // Ansonsten neues Element an das Ende der Liste anhängen
+            prev.next = q;
+        }
+    }
+}
---
+++ Vorlesung-20260421/src/NumberNode.java
@@ -0,0 +1,9 @@
+public class NumberNode {
+
+    public int value;
+    public NumberNode next; // Verweis auf nächstes Element
+
+    public NumberNode(int value) { // Konstruktor
+        this.value = value;
+    }
+}
