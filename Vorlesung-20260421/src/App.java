21c5eaec554c6e006d3fc0ca26306e7f342caf48
Simon Nestler <simon.nestler@thi.de>
2026-04-21 13:04:28 +0200

    Ende der Vorlesung

--- Vorlesung-20260421/src/App.java
+++ Vorlesung-20260421/src/App.java
@@ -1,6 +1,48 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // manuellerWeg();
+
+        NumberList list0 = new NumberList();
+        list0.add(10);
+        list0.print();
+
+        NumberList list1 = new NumberList();
+        list1.add(4);
+        list1.add(3);
+        list1.add(2);
+        list1.add(1);
+        list1.add(10);
+        list1.print();
+
+        NumberList list2 = new NumberList();
+        list2.add(4);
+        list2.add(3);
+        list2.add(2);
+        list2.add(1);
+        list2.add(3);
+        list2.print();
+
+        NumberList list3 = new NumberList();
+        list3.add(4);
+        list3.add(3);
+        list3.add(2);
+        list3.add(1);
+        list3.add(1);
+        list3.print();
+    }
+
+    public static void manuellerWeg() {
         NumberList list = new NumberList();
+
+        NumberNode node1 = new NumberNode(4);
+        NumberNode node2 = new NumberNode(3);
+        NumberNode node3 = new NumberNode(2);
+        NumberNode node4 = new NumberNode(1);
+
+        list.head = node1; // Kopf der Liste auf erstes Element setzen
+        node1.next = node2; // 1. Element zeigt auf 2. Element
+        node2.next = node3; // 2. Element zeigt auf 3. Element
+        node3.next = node4; // 3. Element zeigt auf 4. Element
 
         list.print();
     }
--- Vorlesung-20260421/src/NumberList.java
+++ Vorlesung-20260421/src/NumberList.java
@@ -1,16 +1,17 @@
 public class NumberList {
-    NumberNode head; // 1. Listenelement = Kopf der Liste
+    public NumberNode head; // 1. Listenelement = Kopf der Liste
 
     public void add(int value) {
         NumberNode q = new NumberNode(value);
         NumberNode p = head;
         NumberNode prev = null;
 
-        while (p != null) { // Suche bis zum Ende der Liste
+        while ((p != null) && (p.value < value)) { // Suche bis zum Ende der Liste
             prev = p;
             p = p.next;
         }
 
+        q.next = p;
         if (prev == null) { // Liste war leer, neues Element wird zum Kopf
             head = q;
         } else { // Ansonsten neues Element an das Ende der Liste anhängen
