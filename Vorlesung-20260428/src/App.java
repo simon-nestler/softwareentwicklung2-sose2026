97cf9ed7da34a1a04aad1f56e491b464a30dbbbf
Simon Nestler <simon.nestler@thi.de>
2026-04-28 11:35:45 +0200

    Aufgabe

--- Vorlesung-20260428/src/App.java
+++ Vorlesung-20260428/src/App.java
@@ -1,12 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
-        System.out.println("Rekursion: " + Rekursion.fakultaet(100));
-        long end = System.currentTimeMillis();
+        long start, end;
+        int n = 10000;
+
+        start = System.currentTimeMillis();
+        System.out.println("Rekursion: " + Rekursion.fakultaet(n));
+        end = System.currentTimeMillis();
         System.out.println("Dauer: " + (end - start) + "ms");
 
         start = System.currentTimeMillis();
-        System.out.println("Iteration: " + Iteration.fakultaet(100));
+        System.out.println("Iteration: " + Iteration.fakultaet(n));
         end = System.currentTimeMillis();
         System.out.println("Dauer: " + (end - start) + "ms");
     }
--- Vorlesung-20260428/src/Iteration.java
+++ Vorlesung-20260428/src/Iteration.java
@@ -1,4 +1,5 @@
 public class Iteration {
+
     public static int fakultaet(int n) {
         // TODO
 
