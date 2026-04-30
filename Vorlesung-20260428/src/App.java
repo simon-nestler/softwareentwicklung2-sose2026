6cebd2e7489e7e184da11e1e3f54671735993bcb
Simon Nestler <simon.nestler@thi.de>
2026-04-30 10:06:23 +0200

    Vorlesung Start

--- Vorlesung-20260428/src/App.java
+++ Vorlesung-20260428/src/App.java
@@ -5,8 +5,14 @@ public class App {
     }
 
     public static void baum() {
-        Tree<Integer> links = new Tree<Integer>(5);
-        Tree<Integer> rechts = new Tree<Integer>(2);
+        Tree<Integer> lllinks = new Tree<Integer>(8);
+        Tree<Integer> llinks = new Tree<Integer>(5, lllinks, null);
+        Tree<Integer> rlinks = new Tree<Integer>(1);
+        Tree<Integer> links = new Tree<Integer>(5, llinks, rlinks);
+
+        Tree<Integer> lrechts = new Tree<Integer>(4, new Tree<Integer>(3), new Tree<Integer>(2));
+        Tree<Integer> rrechts = new Tree<Integer>(1);
+        Tree<Integer> rechts = new Tree<Integer>(2, lrechts, rrechts);
 
         Tree<Integer> baum = new Tree<Integer>(10, links, rechts);
 
