eb5fe89431653d255c1e3d3b10944ea48cd855bb
Simon Nestler <simon.nestler@thi.de>
2026-05-07 09:31:31 +0200

    drei buttons und ein label

--- Vorlesung-20260507/src/ZentralisierteErgeignisverarbeitung.java
+++ Vorlesung-20260507/src/ZentralisierteErgeignisverarbeitung.java
@@ -1,19 +1,33 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
 
 public class ZentralisierteErgeignisverarbeitung extends JFrame {
-    private JTextArea textArea;
+    private JButton button1, button2, button3;
+    private JLabel jlabel;
+    private JPanel panel;
 
     public ZentralisierteErgeignisverarbeitung() {
         setTitle("Zentralisierte Ereignisverarbeitung");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
+        button1 = new JButton("Button1");
+        button2 = new JButton("Button2");
+        button3 = new JButton("Button3");
+        jlabel = new JLabel("leer");
 
-    public void logEvent(String event) {
-        textArea.append(event + "\n");
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 2));
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(jlabel);
+
+        add(panel);
     }
 
 }
