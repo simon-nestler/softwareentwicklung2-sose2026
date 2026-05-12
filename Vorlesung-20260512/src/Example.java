8940c993a097a06f38b4c849dfa4c66cb22632a9
Simon Nestler <simon.nestler@thi.de>
2026-05-12 12:37:04 +0200

    0 bis 180

--- Vorlesung-20260512/src/Example.java
+++ Vorlesung-20260512/src/Example.java
@@ -19,20 +19,23 @@ public class Example extends JPanel {
 
         g.drawRect(20, 30, 200, 100);
         g.drawOval(20, 30, 200, 100);
+
+        g.setColor(Color.BLUE);
+        g.fillArc(20, 30, 200, 100, 0, 180);
+
         g.drawLine(20, 30, 220, 130);
 
         g.fillRect(250, 30, 50, 50);
         g.fillOval(320, 30, 50, 50);
 
-        int[] xPoints = { 400, 450, 500 };
-        int[] yPoints = { 30, 80, 30 };
-        g.fillPolygon(xPoints, yPoints, 3);
-
-        int[] xPoints2 = { 400, 450, 500, 600, 200, 150 };
-        int[] yPoints2 = { 150, 200, 150, 175, 175, 150 };
-        g.fillPolygon(xPoints2, yPoints2, 6);
-
-        g.drawArc(250, 400, 100, 100, 0, 180);
-        g.fillArc(450, 400, 100, 100, 0, 180);
+        /*
+         * int[] xPoints = { 400, 450, 500 };
+         * int[] yPoints = { 30, 80, 30 };
+         * g.fillPolygon(xPoints, yPoints, 3);
+         * 
+         * int[] xPoints2 = { 400, 450, 500, 600, 200, 150 };
+         * int[] yPoints2 = { 150, 200, 150, 175, 175, 150 };
+         * g.fillPolygon(xPoints2, yPoints2, 6);
+         */
     }
 }
