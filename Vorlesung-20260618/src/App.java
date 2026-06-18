190b06387a655d8f36e1b8592d57c197beb60ddf
Simon Nestler <simon.nestler@thi.de>
2026-06-18 10:04:19 +0200

    Neues Java-Projekt: mit throws Exception

---
+++ Vorlesung-20260618/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    // Ein neues Projekt hat in der main-Methode den Zusatz "throws Exception"
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
